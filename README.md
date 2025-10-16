bb3157c70d35a516912f02e628d2ffa9b7d11e55
Emagnu <ignisvictorious@gmail.com>
2025-10-16 13:14:34 +0100

    add filesupload

---
+++ README.md
@@ -0,0 +1,2 @@
+Adapted from:
+[File Upload and Save in FastAPI: Step-by-Step Tutorial Sarthi Technology](https://www.youtube.com/watch?v=HpuAFQX3_qE)
--- main.py
+++ main.py
@@ -11,3 +11,8 @@ app: FastAPI = FastAPI()
 @app.post(path="/uploadFile/")
 async def upload_file(file: UploadFile) -> dict[str, str | None]:
     return {"filename": file.filename}
+
+
+@app.post(path="/uploadFiles/")
+async def upload_files(files: list[UploadFile]) -> dict[str, list[str | None]]:
+    return {"filenames": [file.filename for file in files]}
