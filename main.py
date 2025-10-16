e0602fdd4903b469fa90f97fd0ca26b5a1857e36
Emagnu <ignisvictorious@gmail.com>
2025-10-16 13:32:18 +0100

    add savefile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #
 #  Import LIBRARIES
+import os
+
 from fastapi import FastAPI, UploadFile
 
 #  Import FILES
@@ -16,3 +18,16 @@ async def upload_file(file: UploadFile) -> dict[str, str | None]:
 @app.post(path="/uploadFiles/")
 async def upload_files(files: list[UploadFile]) -> dict[str, list[str | None]]:
     return {"filenames": [file.filename for file in files]}
+
+
+@app.post(path="/uploadFileSave/")
+async def upload_file_save(file: UploadFile) -> dict[str, str]:
+    upload_directory: str = "./uploads"
+    file_location: str = f"./{upload_directory}/{file.filename}"
+
+    os.makedirs(name=upload_directory, exist_ok=True)
+
+    with open(file=file_location, mode="wb") as buffer:
+        buffer.write(await file.read())
+
+    return {"message": f"{file.filename} saved at {file_location}"}
